8c1161fc80ef8ad0b5551d0be5b8d0e0a2f50ace
Ken2ker <dangkien040308@gmail.com>
2023-06-12 21:25:01 +0700

    upload

--- pgr.js
+++ pgr.js
@@ -2,26 +2,26 @@ var value = document.querySelector('.progress-value')
 var progress = document.querySelector('.progress-circle')
 var text = document.querySelector('.progress-text')
 var inputText = document.querySelector('.input-text')
-var inputPercent = document.querySelector('.input-percent')
 var button = document.querySelector('.input-btn')
 
 
 button.addEventListener('click', handleProgress) 
 
 function handleProgress ()  {
-     var percent = inputPercent.value 
      var contentText = inputText.value
      text.innerText = contentText 
      var start = 0 ;
-     console.log(progress)
-     var progressID = setInterval( () => {
+     var random = Math.floor(Math.random()*101)
+      
+     var progressID = setInterval( () => { 
          start++
          value.innerText = `${start}%`
-         progress.style.background = `conic-gradient(#880bea ${start * 3.6}deg , #ededed 0deg)`;
-         if (start == percent) {
+         progress.style.background = `conic-gradient(#FF9966 ${start * 3.6}deg , #ededed 0deg)`;
+         if (start == random) {
             clearInterval(progressID)
-            inputPercent.value = ''
             inputText.value = ''
          }
-    },10)
+    },25)
+
+
 } 
